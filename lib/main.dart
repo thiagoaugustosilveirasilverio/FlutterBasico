2bbccba938267193c7f8cbe52993b1d469029b14
Thiago Silvério <thiagocrokao@gmail.com>
2023-04-08 17:40:36 -0300

    Componentizando widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'myapp.dart';
+
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
-      
-    );
-  }
-}
-
-class HomePage extends StatelessWidget{
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return  Scaffold(
-       appBar: AppBar(
-        title: Text('Home'),
-      ),
-      body: Center(
-        child: Text('Flutterando'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-        print('Clicou');
-      },
-      ),
-    );
-  }
-
-}
